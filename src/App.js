313ec088b7c89458e0d8330347a36f0c19b467e5
Kyle Coberly <kyle.coberly@gmail.com>
2020-06-11 13:32:08 -0600

    Gettin classy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isSwitchedOn: false,
+    menu: [],
+  }
+
+  handleLightToggle = () => {
+    this.setState({ isSwitchedOn: !this.state.isSwitchedOn });
+  }
+
+  componentDidMount() {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then(response => response.json())
+      .then(response => {
+        const names = response.results.map(pokemon => pokemon.name)
+        this.setState({ menu: names })
+      })
+  }
+
+  render(){
+    const pokemon = this.state.menu.map(pokemon => {
+      return <li key={pokemon}>{pokemon}</li>
+    })
+    return (
+      <div className="App">
+        <p>The light is { this.state.isSwitchedOn ? "on" : "off" }</p>
+        <button onClick={this.handleLightToggle}>Toggle Light</button>
+        <ul> {pokemon} </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
