c993c35a586a20afb8b04e49a5af25863d5985ed
Lydia Gregory <ljg2gb@virginia.edu>
2020-06-11 14:06:22 -0600

    converts class component to a functional component with hooks

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-class App extends Component {
-  state = {
-    isSwitchedOn: false,
-    menu: [],
+export default function App() {
+  const [isSwitchedOn, setIsSwitchedOn] = useState(false)
+  const [menu, setMenu] = useState([])
+
+
+  // setIsSwitchedOn([false])
+  // setMenu([])
+
+  const handleLightToggle = () => {
+    setIsSwitchedOn(!isSwitchedOn)
   }
 
-  handleLightToggle = () => {
-    this.setState({ isSwitchedOn: !this.state.isSwitchedOn });
-  }
-
-  componentDidMount() {
-    fetch("https://pokeapi.co/api/v2/pokemon")
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/')
       .then(response => response.json())
       .then(response => {
         const names = response.results.map(pokemon => pokemon.name)
-        this.setState({ menu: names })
+        setMenu(names)
       })
-  }
+})
 
-  render(){
-    const pokemon = this.state.menu.map(pokemon => {
+    const pokemon = menu.map(pokemon => {
       return <li key={pokemon}>{pokemon}</li>
     })
+
     return (
       <div className="App">
-        <p>The light is { this.state.isSwitchedOn ? "on" : "off" }</p>
-        <button onClick={this.handleLightToggle}>Toggle Light</button>
+        <p>The light is { isSwitchedOn ? "on" : "off" }</p>
+        <button onClick={handleLightToggle}>Toggle Light</button>
         <ul> {pokemon} </ul>
       </div>
     );
-  }
 }
-
-export default App;
